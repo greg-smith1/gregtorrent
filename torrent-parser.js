34fc06da758027440ec3634d71246c2858958f9e
Greg Smith <greg.smith3492@gmail.com>
2022-10-04 16:42:25 -0400

    add size, infoHash to torrent-parser

--- torrent-parser.js
+++ torrent-parser.js
@@ -1,16 +1,23 @@
 'use strict';
 
 const fs = require('fs');
+const bignum = require('bignum');
 const bencode = require('bencode');
+const crypto = require('crypto');
 
 module.exports.open = (filepath) => {
   return bencode.decode(fs.readFileSync(filepath));
 }
 
 module.exports.size = torrent => {
+  const size = torrent.info.files ?
+    torrent.info.files.map(file => file.length).reduce((a, b) => a + b) :
+    torrent.info.length;
 
+  return bignum.toBuffer(size, {size: 8});
 }
 
 module.exports.infoHash = torrent => {
-
+  const info = bencode.encode(torrent.info);
+  return crypto.createHash('sha1').update(info).digest();
 }
